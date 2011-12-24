91b852bfaeabc2e9a61f94e7753e2c44a8476dd0
Francesco <f@mazzo.li>
2011-12-24 16:21:47 +0000

    Added some files

---
+++ hs/test/test.ml
@@ -0,0 +1,48 @@
+
+let s = \x y z -> x z (y z)
+
+let k = \x y -> x
+
+let i = s k k
+
+let pair = \x y -> (x, y)
+
+let triple = \x y z -> (x, y, z)
+
+let curry = \f x y -> f (x, y)
+
+let uncurry = \f (x, y) -> f x y
+
+let fst = \(x, y) -> x
+
+let snd = \(x, y) -> y
+
+let foo = let x = 4 in
+          let y = 0 in
+          plus x y
+
+data Maybe a =
+    Just a
+  | Nothing
+
+let fromJust = \(Just x) -> x
+
+let fromMaybe = \x m -> case m of
+                            Just y -> y
+                          | Nothing -> x
+
+let return = Just
+
+let bind = \m f -> case m of
+                     Just x  -> f x
+                   | Nothing -> Nothing
+
+let join = \m -> case m of
+                    Just (Just x) -> Just x
+                  | Nothing       -> Nothing
+
+let subtract = \x y -> plus x (negate y)
+
+let times = \x y -> case isZero y of
+                       True -> 0
+                     | False -> plus x (times x (subtract y 1))

5a59114d731817a75d4fe6dce8c92b8ed2ec0320
Francesco <f@mazzo.li>
2011-12-28 15:14:41 +0000

    Removed sml part

---
+++ Load.hs
@@ -0,0 +1,19 @@
+module Load
+       ( module Syntax
+       , module Lexer
+       , module Parser
+       , module Desugar
+       , module TI.TypesTypes
+       , module TI.BaseEnv
+       , module TI.TypeInfer
+       , module Pretty
+       ) where
+
+import Syntax
+import Lexer
+import Parser
+import Desugar
+import TI.TypesTypes
+import TI.BaseEnv
+import TI.TypeInfer
+import Pretty
